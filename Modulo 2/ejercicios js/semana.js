49174b4d316b74575658f327effdbc09122beb2d
keiler99 <santiagobracho90@gmail.com>
2025-06-04 16:55:34 -0400

    Add files via upload

---
+++ Modulo 2/ejercicios js/semana.js
@@ -0,0 +1,29 @@
+let dia = 5;
+let nombredia;
+
+switch(dia){
+    case 1:
+        nombredia = "lunes";
+        break;
+    case 2:
+        nombredia = "martes";
+        break;
+    case 3:
+        nombredia = "miercoles";
+        break;
+    case 4:
+        nombredia = "jueves";
+        break;
+    case 5:
+        nombredia = "viernes";
+        break;
+    case 6:
+        nombredia = "sabado";
+        break;
+    case 7:
+        nombredia = "domingo";
+        break;
+    default:
+        nombredia = "dia invalido";
+}
+console.log(nombredia)
